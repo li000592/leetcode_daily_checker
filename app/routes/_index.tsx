3ea462ddee5fb6cb0ba95bb1b6f8a0c700608c2c
li000592 <li000592@algonquinlive.com>
2024-10-18 19:46:34 -0400

    add loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
 import type { MetaFunction } from "@netlify/remix-runtime";
+import { json } from "@remix-run/node";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,7 +13,49 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  try {
+    const response = await fetch("https://leetcode.com/graphql/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query recentAcSubmissions($username: String!, $limit: Int!) {
+            recentAcSubmissionList(username: $username, limit: $limit) {
+              id
+              title
+              titleSlug
+              timestamp
+            }
+          }
+        `,
+        variables: {
+          username: "li000592", // Replace with the desired username
+          limit: 15,
+        },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    const submissions = data.data.recentAcSubmissionList;
+
+    return json(submissions); // Return the submissions in JSON format
+  } catch (error) {
+    console.error("Failed to fetch submissions:", error);
+    // Return an error response if fetching fails
+    return json({ error: "Failed to fetch data" }, { status: 500 });
+  }
+};
 export default function Index() {
+  const invoice = useLoaderData<typeof loader>();
+  console.log(invoice);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
